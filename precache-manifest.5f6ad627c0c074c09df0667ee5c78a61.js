00d915ae7f76f89782de452a36d599f288e6b057
alisabaj <alisabaj@gmail.com>
2020-02-06 11:21:01 -0800

    Updates

---
+++ precache-manifest.5f6ad627c0c074c09df0667ee5c78a61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b66a2908e9cc898009964d1b1333a887",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4c50867541b0a838860",
+    "url": "/static/css/main.31ab34ec.chunk.css"
+  },
+  {
+    "revision": "d52bd554546983b9e242",
+    "url": "/static/js/2.9aba04f4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.9aba04f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4c50867541b0a838860",
+    "url": "/static/js/main.ff2a5e9b.chunk.js"
+  },
+  {
+    "revision": "80e1376b1f84ed0cc164",
+    "url": "/static/js/runtime-main.02cf2b96.js"
+  },
+  {
+    "revision": "61c1657feeb8fb2cd6bc645ad687dbf5",
+    "url": "/static/media/alisa-bajramovic-profile.61c1657f.jpg"
+  }
+]);
